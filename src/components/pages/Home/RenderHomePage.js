cb91e2248fb44a7dd7ff804b3dbb6ccb2ef965f1
Sam Kester <samkester@gmail.com>
2020-12-08 16:04:19 -0700

    home page layout; much wrestling with antd

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../../common';
+import { Layout, Typography, Space, Divider } from 'antd';
+
+const { Paragraph, Title } = Typography;
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
   return (
-    <div>
-      <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
-      <div>
-        <p>
-          This is an example of a common example of how we'd like for you to
-          approach components.
-        </p>
-        <p>
-          <Link to="/profile-list">Profiles Example</Link>
-        </p>
-        <p>
-          <Link to="/example-list">Example List of Items</Link>
-        </p>
-        <p>
-          <Link to="/datavis">Data Visualizations Example</Link>
-        </p>
-        <p>
-          <Button
-            handleClick={() => authService.logout()}
-            buttonText="Logout"
-          />
-        </p>
-      </div>
-    </div>
+    <Layout>
+      <Layout.Header>
+        <Space direction="vertical" align="center" style={{ width: '100%' }}>
+          <Space size="large" split={<Divider type="vertical" />}>
+            <Title>Microfund</Title>
+            <Paragraph>{userInfo.name}</Paragraph>
+            <Paragraph>
+              <Link to="/">Dashboard</Link>
+            </Paragraph>
+            <Paragraph>
+              <Link onClick={() => authService.logout()}>Log Out</Link>
+            </Paragraph>
+          </Space>
+        </Space>
+      </Layout.Header>
+      <Layout.Content>
+        <Space direction="vertical" align="center" style={{ width: '100%' }}>
+          <Paragraph>
+            Replace this paragraph with a dashboard component.
+          </Paragraph>
+          <Paragraph>
+            Replace this paragraph with a dashboard component.
+          </Paragraph>
+          <Paragraph>
+            Replace this paragraph with a dashboard component.
+          </Paragraph>
+          <Paragraph>
+            Replace this paragraph with a dashboard component.
+          </Paragraph>
+          <Paragraph>
+            Replace this paragraph with a dashboard component.
+          </Paragraph>
+        </Space>
+      </Layout.Content>
+    </Layout>
   );
 }
 export default RenderHomePage;
