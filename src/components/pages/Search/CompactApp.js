8391d6e33385ac0ecad2d99fe6bdfb41cd241648
Fatima Rizvi <fatimarzv23@gmail.com>
2021-01-21 17:20:21 -0600

    MOdified the modal to have Accept/Reject buttons

--- src/components/pages/Search/CompactApp.js
+++ src/components/pages/Search/CompactApp.js
@@ -6,7 +6,10 @@ import { Modal, Button } from 'antd';
 function CompactApp({ app }) {
   console.log(app);
 
-  const [modalState, setModalState] = useState({ visible: false });
+  const [modalState, setModalState] = useState({
+    visible: false,
+    loading: false,
+  });
 
   const showModal = () => {
     setModalState({
@@ -16,12 +19,14 @@ function CompactApp({ app }) {
 
   const handleOk = e => {
     console.log(e);
-    setModalState({
-      visible: false,
-    });
+
+    setModalState({ loading: true });
+    setTimeout(() => {
+      setModalState({ loading: false, visible: false });
+    }, 3000);
   };
 
-  const handleCancel = e => {
+  const handleSubmit = e => {
     console.log(e);
     setModalState({
       visible: false,
@@ -29,30 +34,33 @@ function CompactApp({ app }) {
   };
 
   return (
-    //   <div>
-    //     <h3> Compact App </h3>
-    //     <h4>{app.name}</h4>
-    //     {/* <h5>{app.organization.name}</h5> */}
-    //     <h5>{app.status}</h5>
-    //   </div>
-    // <Card title = {app.name}>
-
-    // </Card>
     <div>
       <Button type="primary" onClick={showModal}>
-        {app.name} {app.organization.name} {app.status}
+        Open Modal with customized footer
       </Button>
       <Modal
-        title="Basic Modal"
         visible={modalState.visible}
+        title="Title"
         onOk={handleOk}
-        onCancel={handleCancel}
-        okButtonProps={{ disabled: true }}
-        cancelButtonProps={{ disabled: true }}
+        onCancel={handleSubmit}
+        footer={[
+          <Button key="back" onClick={handleSubmit}>
+            {' '}
+            {/*Change handleSubmit to later work to reject apps*/}
+            Reject
+          </Button>,
+          <Button
+            key="submit"
+            type="primary"
+            loading={modalState.loading}
+            onClick={handleOk}
+          >
+            Accept
+          </Button>,
+        ]}
       >
-        <h3>{app.name}</h3>
-        <h4>Org: {app.organization.name}</h4>
-        <h4>Status: {app.status}</h4>
+        <h3>Org: {app.organization.name}</h3>
+        <h3>Status: {app.status}</h3>
         <p>Reason: {app.reason}</p>
       </Modal>
     </div>
@@ -60,3 +68,30 @@ function CompactApp({ app }) {
 }
 
 export default CompactApp;
+
+//   <div>
+//     <h3> Compact App </h3>
+//     <h4>{app.name}</h4>
+//     {/* <h5>{app.organization.name}</h5> */}
+//     <h5>{app.status}</h5>
+//   </div>
+// <Card title = {app.name}>
+
+// </Card>
+
+/* <Button type="primary" onClick={showModal}>
+    {app.name} {app.organization.name} {app.status}
+  </Button>
+  <Modal
+    title={app.name}
+    centered
+    visible={modalState.visible}
+    onOk={handleOk}
+    onCancel={handleCancel}
+    okButtonProps={{ disabled: true }}
+    cancelButtonProps={{ disabled: true }}
+  >
+    <h3>Org: {app.organization.name}</h3>
+    <h3>Status: {app.status}</h3>
+    <p>Reason: {app.reason}</p>
+  </Modal> */
