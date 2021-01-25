30351a15f37514f3154bc70b69abc756d244e671
Trevor Buchanan <trevor@trevorbuchanan.com>
2021-01-25 14:39:17 -0600

    cleaned up the user react-query hook, and added one to get organization applications

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,40 +1,41 @@
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 import axiosWithAuth from '../utils/axiosWithAuth';
 
+/*
+React-query hook to get and update the current users information. Should be able to get all the information about the user, including their own applications, and keep track of it with this.
+*/
+
 // GET request for /users/getuserinfo
 const getUserInfo = async auth => {
   const token = auth.authState.accessToken;
   const response = await axiosWithAuth(token).get(`/users/getuserinfo`);
   return response;
 };
-// react query request to getUserInfo and keep in 'currentUser'
-// this will return an object with various fields
-// among things like `isLoading` and `error`
-// the returned data is in `data`
-export function useUserInfo(auth) {
-  return useQuery('currentUser', () => getUserInfo(auth));
-}
+
 //  PATCH request to update userinfo
 export function patchUser(auth, userData) {
   const token = auth.authState.accessToken;
   return axiosWithAuth(token).patch(`users/user/${userData.userid}`, userData);
 }
+
 // returns a mutation function that will update server state
 // with the object given as an argument
 // it will then invalidate 'currentUser' triggering an automatic
 // update to the server state 'currentUser' kept by react query.
-export function useTheMutation(auth) {
+export function useMutationforUser(auth) {
   const queryClient = useQueryClient();
   return useMutation(userData => patchUser(auth, userData), {
     onSuccess: () => {
-      queryClient.invalidateQueries('currentUser');
+      queryClient.invalidateQueries('user');
     },
   });
 }
 
 // Function to return the query and mutation in an array
-// I feel like this makes it very similar to setState..
-// but is it actually a good idea?
+// index 0 will be an object with various fields including
+// 'isLoading', 'error', and data in 'data'
+// index 1 will be the mutation that can be invoked via
+// .mutate(userData)
 export function useUserHook(auth) {
-  return [useUserInfo(auth), useTheMutation(auth)];
+  return [useQuery('user', () => getUserInfo(auth)), useMutationforUser(auth)];
 }
---
+++ src/hooks/queryapps.js
@@ -0,0 +1,45 @@
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+/*
+react-query hook to keep track of multiple apps. The idea is that main admins and partners need to be able to review submitted applications. So we have a query key ['Apps', orgid] to store apps to that org, and the ability to PATCH them to update the status (like accepted, declined, etc)
+*/
+
+// GET request for applications to specified org.
+async function getApps(auth, orgid) {
+  const token = auth.authState.accessToken;
+  const response = await axiosWithAuth(token).get(`/orgs/org/${orgid}/apps`);
+  return response;
+}
+
+// PATCH for an application
+function patchApp(auth, appData) {
+  const token = auth.authState.accessToken;
+  console.log(appData);
+  return axiosWithAuth(token).patch(`apps/app/${appData.appid}`, appData);
+}
+
+// returns a mutation function that will update server state
+// with the object given as an argument. it will then
+// invalidate ['Appsinfo', orgid] triggering an automatic
+// update to the server state kept by react query.
+export function useMutationForApp(auth, orgid) {
+  const queryClient = useQueryClient();
+  return useMutation(appData => patchApp(auth, appData), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['apps', orgid]);
+    },
+  });
+}
+
+// Function to return the query and mutation in an array
+// index 0 will be an object with various fields including
+// 'isLoading', 'error', and returned data in 'data'
+// index 1 will be the mutation that can be invoked via
+// .mutate(appData)
+export function useAppsQuery(auth, orgid) {
+  return [
+    useQuery(['apps', orgid], () => getApps(auth, orgid)),
+    useMutationForApp(auth, orgid),
+  ];
+}
