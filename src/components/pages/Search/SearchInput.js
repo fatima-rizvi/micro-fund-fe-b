defb7c183be0296ef4135ffc41986a1fde78417d
Fatima Rizvi <fatimarzv23@gmail.com>
2021-01-29 08:17:21 -0600

    Created SearchInput component and iportant necessary antD components

---
+++ src/components/pages/Search/SearchInput.js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from 'react';
+// import {} from "antd";
+import ReactDOM from 'react-dom';
+import { Form, Input, Button, Select, Space } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+
+const { Search } = Input;
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1890ff',
+    }}
+  />
+);
+
+const onSearch = value => console.log(value);
+
+const { Option } = Select;
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+const SearchInput = () => {
+  const [form] = Form.useForm();
+
+  const onGenderChange = value => {
+    switch (value) {
+      case 'male':
+        form.setFieldsValue({
+          note: 'Hi, man!',
+        });
+        return;
+
+      case 'female':
+        form.setFieldsValue({
+          note: 'Hi, lady!',
+        });
+        return;
+
+      case 'other':
+        form.setFieldsValue({
+          note: 'Hi there!',
+        });
+        return;
+    }
+  };
+
+  const onFinish = values => {
+    console.log(values);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onFill = () => {
+    form.setFieldsValue({
+      note: 'Hello world!',
+      gender: 'male',
+    });
+  };
+
+  return (
+    <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+      <Form.Item
+        name="name"
+        label="Name"
+        rules={[
+          {
+            required: false,
+          },
+        ]}
+      >
+        <Search
+          placeholder="input name"
+          onSearch={onSearch}
+          style={{ width: 200 }}
+        />
+      </Form.Item>
+      <Form.Item
+        name="status"
+        label="Status"
+        rules={[
+          {
+            required: false,
+          },
+        ]}
+      >
+        <Select
+          placeholder="Select a option and change input text above"
+          onChange={onGenderChange}
+          allowClear
+        >
+          <Option value="male">male</Option>
+          <Option value="female">female</Option>
+          <Option value="other">other</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        noStyle
+        shouldUpdate={(prevValues, currentValues) =>
+          prevValues.gender !== currentValues.gender
+        }
+      >
+        {({ getFieldValue }) => {
+          return getFieldValue('gender') === 'other' ? (
+            <Form.Item
+              name="customizeGender"
+              label="Customize Gender"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          ) : null;
+        }}
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          Reset
+        </Button>
+        <Button type="link" htmlType="button" onClick={onFill}>
+          Fill form
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default SearchInput;
