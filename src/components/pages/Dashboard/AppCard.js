a4af7a0f4cb76d1efa43135aa489e65d448cc9b3
Trevor Buchanan <trevor@trevorbuchanan.com>
2021-01-29 16:07:03 -0600

    removed custom stylings, fixed form, and wrote working axios POST

--- src/components/pages/Dashboard/AppCard.js
+++ src/components/pages/Dashboard/AppCard.js
@@ -1,121 +1,118 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-// import Form from '../../common/Form';
-// import UserDescription from '../../common/UserDescription';
-// import logo from '../../../images/microLogo.png';
+import { useOktaAuth } from '@okta/okta-react';
+import { useQueryClient } from 'react-query';
+import axiosWithAuth from '../../../utils/axiosWithAuth';
 
+// A custom POST for apps
+// We can't use a react query, because that's for handling
+// Server state, and this doesn't exist in server state yet
+function postAppData(auth, appData) {
+  return (
+    auth.authService
+      .getAccessToken()
+      .then(token => {
+        return axiosWithAuth(token).post(
+          `/apps/app/new`,
+          JSON.stringify(appData),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+      })
+      // the user's applications are in the 'user' query
+      // so we, confusingly, need to invalidate that
+      .catch(error => console.error(error))
+  );
+}
 
-
-function AppCard() {
-  // Start of styled-components
-
-  const StyledHeadingContainer = styled.div`
-  color: black;
-  text-align: center;
-  width: 80%;
-  margin: 0 auto;
-  `;
-
-
-  const StyledFormContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin: 0 auto;
-  width: 80%;
-  height: 100%;
-  
-  `;
-
-
-  const StyledFormSubmitButton = styled.button `
-  width: 18%; 
-  background-color: #6558F5;
-  border-radius: 8%;
-  `;
-
-
-
-  // End of styled-components
-  
-  
-  
-  
+// This is just completely wrong.
+function AppCard({ orgId }) {
+  const auth = useOktaAuth();
+  const queryClient = useQueryClient();
   const [formState, setFormState] = useState({
-    name:"",
-    phoneNumber:"",
-    address: "",
-    description: ""
+    name: '',
+    phone: '',
+    address: '',
+    reason: '',
   });
-
 
   const handleChanges = e => {
     let name = e.target.name;
     let value = e.target.value;
-    setFormState({...formState, [name]: value});
-}; 
-
+    setFormState({ ...formState, [name]: value });
+  };
 
   const onSubmit = e => {
     // React query to submit an application
+    e.preventDefault();
+    postAppData(auth, {
+      ...formState,
+      organization: { orgid: 4 },
+    });
+    queryClient.invalidateQueries('user');
   };
-
 
   return (
     <>
-    <StyledHeadingContainer class="application-container">
-      <h1> Org Name </h1>
-      <p> userInput_id Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat
-        urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu
-        venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget.
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos. Duis pharetra luctus lacus ut vestibulum. Maecenas
-        ipsum lacus, lacinia quis posuere ut, pulvinar vitae dolor. Integer eu
-        nibh at nisi ullamcorper sagittis id vel leo. Integer feugiat faucibus
-        libero, at maximus nisl suscipit posuere. Morbi nec enim nunc. Phasellus
-        bibendum turpis ut ipsum egestas, sed sollicitudin elit convallis. Cras
-        pharetra mi tristique sapien vestibulum lobortis. Nam eget bibendum
-        metus, non dictum mauris. Nulla at tellus sagittis, viverra est a,
-        bibendum metus.</p>
-    </StyledHeadingContainer>
+      <div>
+        <h1> Org Name </h1>
+        <p>
+          {' '}
+          userInput_id Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat
+          urna. Morbi a bibendum metus. Donec scelerisque sollicitudin enim eu
+          venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget.
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+          per inceptos himenaeos. Duis pharetra luctus lacus ut vestibulum.
+          Maecenas ipsum lacus, lacinia quis posuere ut, pulvinar vitae dolor.
+          Integer eu nibh at nisi ullamcorper sagittis id vel leo. Integer
+          feugiat faucibus libero, at maximus nisl suscipit posuere. Morbi nec
+          enim nunc. Phasellus bibendum turpis ut ipsum egestas, sed
+          sollicitudin elit convallis. Cras pharetra mi tristique sapien
+          vestibulum lobortis. Nam eget bibendum metus, non dictum mauris. Nulla
+          at tellus sagittis, viverra est a, bibendum metus.
+        </p>
+      </div>
 
-    <StyledFormContainer>
-      <form id="org-applcation-form">
-      <input 
-      name="name"
-      value={formState.name}
-      onChange={handleChanges}
-      placeholder="Name"
-      />
+      <div>
+        <form id="org-applcation-form">
+          <input
+            name="name"
+            value={formState.name}
+            onChange={handleChanges}
+            placeholder="Name"
+          />
 
-      <input 
-      name="phoneNumber"
-      value={formState.phoneNumber}
-      onChange={handleChanges}
-      placeholder="Phone Number"
-      />
+          <input
+            name="phone"
+            value={formState.phone}
+            onChange={handleChanges}
+            placeholder="Phone Number"
+          />
 
-      <input 
-      name="address"
-      value={formState.address}
-      onChange={handleChanges}
-      placeholder="Address"
-      />
+          <input
+            name="address"
+            value={formState.address}
+            onChange={handleChanges}
+            placeholder="Address"
+          />
 
-      <input 
-      name="description"
-      value={formState.description}
-      onChange={handleChanges}
-      placeholder="Description"
-      />
+          <input
+            name="reason"
+            value={formState.reason}
+            onChange={handleChanges}
+            placeholder="Reason"
+          />
 
-      <StyledFormSubmitButton> Apply </StyledFormSubmitButton>
-      </form>
-    </StyledFormContainer>
+          <button onClick={onSubmit}> Apply </button>
+        </form>
+      </div>
     </>
   );
 }
-
 
 export default AppCard;
